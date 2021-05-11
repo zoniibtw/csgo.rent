94a1ea48a620f2ca1ea5852d4889c1cd0489f6c5
Anton Persson <antonsomoza@hotmail.com>
2021-05-12 01:54:32 +0200

    home-page / components
    I have worked on the home pages components and started code some html and css.
    
    Still have some features left to fix:
    - Footer
    - Background-img for home page
    - About us part
    - Make it responsible for phone users.

--- src/components/Home/section-three.js
+++ src/components/Home/section-three.js
@@ -1,9 +1,38 @@
-import './footer.css';
+import './css/section-three.css';
 
 function SectionThree() {
   return (
-    <div className="Footer">
+    <div className="section-three">
+      <div className="about-us-container">
+        <div className="about-title">
+          <h1>Vem/vilka består vi av?</h1>
+        </div>
+        <div className="management-container">
+          <div className="management-item">
+            <img></img>
+            <div className="management-info">
+              <h3>Niklas Jörgensen</h3>
+              <p>Projekt Ledare</p>
+            </div>
+          </div>
 
+          <div className="management-item">
+            <img></img>
+            <div className="management-info">
+              <h3>Anton Persson</h3>
+              <p>Grundare & Ägare</p>
+            </div>
+          </div>
+
+          <div className="management-item">
+            <img></img>
+            <div className="management-info">
+              <h3>Eric Jörgensen</h3>
+              <p>Redovisningsekonom</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
