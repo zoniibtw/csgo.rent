71c0dacd362c3721c1e970cf0a37e5fcdf512a3d
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-05-27 16:15:31 +0200

    updated connection to phpmyadmin

--- Config/Database.php
+++ Config/Database.php
@@ -2,10 +2,10 @@
 
 Class Database {
     //DB Params
-    private $host = 'localhost';
+    private $host = '206.81.27.23';
     private $db_name = 'csgorent';
-    private $username = 'root';
-    private $password = 'Hacked!@#$';
+    private $username = 'admin';
+    private $password = 'f87f7cb906f269d7bca1d4d8df380c253700d1aa9cec832b';
     private $conn;
     //DB Connect
     public function connect(){
