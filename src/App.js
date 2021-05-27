626051b6637b58bbc26bda6f8fddc67333d1c3a2
Anton Persson <antonsomoza@hotmail.com>
2021-05-27 16:42:02 +0200

    css changes
    css changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Header-Footer/footer';
 import Home from './pages/Home';
 import Market from './pages/Market';
 import Settings from './pages/Settings';
-import Test from './pages/test';
+import Avtal from './pages/avtal';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 class App extends Component {
@@ -42,9 +42,9 @@ class App extends Component {
         <Router>
             <div className="App">
                 <Switch>
-                    <Route path="/test" exact component={Test} />
                     <Route path="/market" component={Market} />
                     <Route path="/settings" component={Settings} />
+                    <Route path="/avtal" component={Avtal} />
                     <Route path="/" component={Home} />
                 </Switch>
                 <Footer />
---
+++ src/pages/avtal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Avtal() {
+  return (
+    <div className="avtal">
+        <h1>Avtal</h1>
+    </div>
+  );
+}
+
+export default Avtal;
