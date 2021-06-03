980d241a06386b378bb70d93d2b1bfe0b73c3717
Anton Persson <antonsomoza@hotmail.com>
2021-06-03 03:06:26 +0200

    changes
    changes

--- src/pages/Market.js
+++ src/pages/Market.js
@@ -9,31 +9,19 @@ function Market() {
   return (
       <div className="Market">
         <Header />
-        <div className="search">
-          <div className="search-container">
-            <div className="search-filter">
-              <div className="search-price-option"> 
-                <div className="search-bar"></div>
-                <div className="price-range"></div>
-                <div className="item-ban-range"></div>
-              </div>
-              <div className="filter-option">
-                <div className="option-one">
-                  <div className="weapon-option"></div>
-                  <div className="weapon-option"></div>
-                  <div className="weapon-option"></div>
-                  <div className="weapon-option"></div>
-                  <div className="weapon-option"></div>
-                  <div className="weapon-option"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div className="items-section">
-          <div className="items-container">
-            <Item></Item>
+          <div className="items-container" >
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
           </div>
         </div>
 
