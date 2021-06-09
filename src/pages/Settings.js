884bb93cfab690abd1fdc70920efd4faef2d4c78
Anton Persson <antonsomoza@hotmail.com>
2021-06-09 20:58:40 +0200

    changes for settings, and item.css

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -7,15 +7,30 @@ function Settings() {
   return (
     <div className="Settings">
       <Header />
-      <div className="left-navbar">
-        <ul>
-          <li><img /><p></p></li>
-          <li><img /><p></p></li>
-          <li><img /><p></p></li>
-        </ul>
-      </div>
-      <div className="settings-overall">
-        <div className=""></div>
+      <div className="settings-page">
+        <div className="left-navbar">
+          <ul>
+            <li><img /><p>Personlig info</p></li>
+            <li><img /><p>Transaktioner</p></li>
+          </ul>
+        </div>
+        <div className="settings-container">
+          <div className="personal-settings">
+            <div className="row-system">
+              <div className="first-row-section">
+                <div className="profile"></div>
+                <div className="subscription"></div>
+              </div>
+              <div className="second-row-section">
+                <div className="general-information"></div>
+              </div>
+            </div>
+          </div>
+          <div className="transactions-settings">
+            <div className="transaction-header"></div>
+            <div className=""></div>
+          </div>
+        </div>
       </div>
     </div>
   );
