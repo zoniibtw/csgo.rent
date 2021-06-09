bb7a8760befca214a7acdcbf5e19bada6bb6e1aa
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-06-10 01:38:28 +0200

    fixed login popup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
 import './App.css';
-import './components/Header-Footer/css/popup.css';
 import Footer from './components/Header-Footer/footer';
 import Home from './pages/Home';
 import Market from './pages/Market';
 import Settings from './pages/Settings';
 import Avtal from './pages/avtal';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
-import Popup from './Popup';
 
 class App extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-          showPopup: false
-        };
-      }
-      
-      togglePopup() {
-        this.setState({
-          showPopup: !this.state.showPopup
-        });
-      }
 
     render() {
         return (
         <Router>
             <div className="App">
-          <button onClick={this.togglePopup.bind(this)}>show popup</button>
-          {this.state.showPopup ? <Popup text='Close Me' closePopup = {this.togglePopup.bind(this)} /> : null}
                 <Switch>
                     <Route path="/market" component={Market} />
                     <Route path="/settings" component={Settings} />
--- src/components/Header-Footer/header-home.js
+++ src/components/Header-Footer/header-home.js
@@ -1,19 +1,41 @@
 import './css/header-home.css';
-import logo from '../../img/logo.svg';
+//import logo, { ReactComponent } from '../../img/logo.svg';
+import Popup from '../../Popup';
+import './css/popup.css';
+import React, { Component } from 'react';
 
-function HeaderHome() {
-  return (
+class HeaderHome extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false
+    };
+  }
+  
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
+    render() {
+      return (
     <div className="header">
       <div className="header-container">
         <div className="logo-type">
           <a>CSGO.RENT</a>
         </div>
         <div className="log-in">
-          <button className="login-button">Logga in med BankID</button>
+        <button onClick={this.togglePopup.bind(this)} className="login-button">Logga in med BankID</button>
+          {this.state.showPopup ? <Popup text='Close Me' closePopup = {this.togglePopup.bind(this)} /> : null}
         </div>
       </div>
     </div>
-  );
+        );
+
+      }
+
 }
 
 export default HeaderHome;
