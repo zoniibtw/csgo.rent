b82055c555d623b20204f933cb3daec5f4bc8c4f
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-06-20 01:33:07 +0200

    fixing errors

--- API/User/createUser.php
+++ API/User/createUser.php
@@ -5,8 +5,8 @@
   header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-  include_once '../../config/Database.php';
-  include_once '../../models/user.php';
+  include_once '../../Config/Database.php';
+  include_once '../../Models/user.php';
 
   // Instantiate DB & connect
   $database = new Database();
--- Config/Database.php
+++ Config/Database.php
@@ -1,24 +1,23 @@
-<?php
-
-Class Database {
-    //DB Params
+<?php 
+  class Database {
+    // DB Params
     private $host = '206.81.27.23';
     private $db_name = 'csgorent';
     private $username = 'admin';
     private $password = 'f87f7cb906f269d7bca1d4d8df380c253700d1aa9cec832b';
     private $conn;
-    //DB Connect
-    public function connect(){
-        $this->conn = null;
 
-        try{
-            $this->conn = NEW PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $e){
-            echo 'Connection error: ' . $e->getMessage();
-        }
-        return $this->conn;
+    // DB Connect
+    public function connect() {
+      $this->conn = null;
+
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
+
+      return $this->conn;
     }
-}
-
-?>
+  }
