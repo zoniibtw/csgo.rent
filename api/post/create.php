bf7cd30d9bbc4df6486538d2504a9210b4fd5237
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-06-20 02:28:38 +0200

    test

--- api/post/create.php
+++ api/post/create.php
@@ -8,6 +8,8 @@
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
 
+  echo 'hej';
+
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
