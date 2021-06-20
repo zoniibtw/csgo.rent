2a39e4fe9016cc6879a88ecac1ad2dac83dea00e
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-06-20 05:19:44 +0200

    reverted

--- Models/user.php
+++ Models/user.php
@@ -163,6 +163,39 @@ Class User {
     return false;
     
   }*/
+<<<<<<< HEAD
+=======
+
+  public function createUser() {
+    // Create query
+    $query = 'INSERT INTO ' . $this->table . ' SET firstName = :firstName, middleName = :middleName, lastName = :lastName, personNummer = :personNummer';
+
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+
+    // Clean data
+    $this->firstName = htmlspecialchars(strip_tags($this->firstName));
+    $this->middleName = htmlspecialchars(strip_tags($this->middleName));
+    $this->lastName = htmlspecialchars(strip_tags($this->lastName));
+    $this->personNummer = htmlspecialchars(strip_tags($this->personNummer));
+
+    // Bind data
+    $stmt->bindParam(':firstName', $this->firstName);
+    $stmt->bindParam(':middleName', $this->middleName);
+    $stmt->bindParam(':lastName', $this->lastName);
+    $stmt->bindParam(':personNummer', $this->personNummer);
+
+    // Execute query
+    if($stmt->execute()) {
+      return true;
+}
+
+// Print error if something goes wrong
+printf("Error: %s.\n", $stmt->error);
+
+return false;
+}
+>>>>>>> parent of 1328c5f8 (revert)
     
 }
 ?>
