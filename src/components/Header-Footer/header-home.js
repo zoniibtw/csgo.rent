c2294be6903b2c8ec68909b642c9bf781b2cbf56
Anton Persson <antonsomoza@hotmail.com>
2021-06-22 04:29:04 +0200

    header logotype clickable

--- src/components/Header-Footer/header-home.js
+++ src/components/Header-Footer/header-home.js
@@ -1,6 +1,7 @@
 import './css/header-home.css';
 import Login from '../pop-up/login-popup'
 import '../pop-up/css/popup.css';
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 
 class HeaderHome extends React.Component {
@@ -11,29 +12,29 @@ class HeaderHome extends React.Component {
       showPopup: false
     };
   }
-  
+
   togglePopup() {
     this.setState({
       showPopup: !this.state.showPopup
     });
   }
 
-    render() {
-      return (
-    <div className="header">
-      <div className="header-container">
-        <div className="logo-type">
-          <a>CSGO.RENT</a>
-        </div>
-        <div className="log-in">
-        <button onClick={this.togglePopup.bind(this)} className="login-button">Logga in med BankID</button>
-          {this.state.showPopup ? <Login text='Close Me' closePopup = {this.togglePopup.bind(this)} /> : null}
+  render() {
+    return (
+      <div className="header-home">
+        <div className="header-container">
+          <div className="logo-type">
+            <a><Link to="/home">CSGO.RENT</Link></a>
+          </div>
+          <div className="log-in">
+            <button onClick={this.togglePopup.bind(this)} className="login-button">Logga in med BankID</button>
+            {this.state.showPopup ? <Login text='Close Me' closePopup={this.togglePopup.bind(this)} /> : null}
+          </div>
         </div>
       </div>
-    </div>
-        );
+    );
 
-      }
+  }
 
 }
 
--- src/components/Header-Footer/header.js
+++ src/components/Header-Footer/header.js
@@ -6,12 +6,12 @@ function Header() {
     <div className="header">
       <div className="header-container">
         <div className="logo-type">
-          <a>CSGO.RENT</a>
+          <a><Link to="/home">CSGO.RENT</Link></a>
         </div>
         <ul className="header-links">
           <li><Link className="header-link-text" to="/home">HOME</Link></li>
-          <li><Link className="header-link-text" to="/home">MARKET</Link></li>
-          <li><Link className="header-link-text" to="/home">SUBSCRIPTIONS</Link></li>
+          <li><Link className="header-link-text" to="/market">MARKET</Link></li>
+          <li><Link className="header-link-text" to="/settings">SUBSCRIPTIONS</Link></li>
         </ul>
         <div className="header-account">
           <div className="header-nickname"></div>
