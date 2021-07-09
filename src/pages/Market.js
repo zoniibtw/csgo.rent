3ea755dedad9aaa0baa82ed93c91a303c0f80be1
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-07-10 01:31:52 +0200

    Update Market.js

--- src/pages/Market.js
+++ src/pages/Market.js
@@ -32,7 +32,7 @@ function Market(){
           <div className="items-section">
             <div className="items-container" >
               <input type="text" placeholder="Search..." value={name} onChange={e => setName(e.target.value)} />
-              <p>{data.map((item) => item.name)}</p>
+              <p>{data.map((item) => item.name)}</p><br></br>
               <Item />
             </div>
           </div>
